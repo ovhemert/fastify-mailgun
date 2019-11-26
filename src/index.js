7cd06144d1f2ec65aad9c109e0153a31b68d6e42
Osmond van Hemert <osmond@vanhemert.nu>
2019-11-26 20:03:43 +0100

    add stored message fetching

--- src/index.js
+++ src/index.js
@@ -2,28 +2,34 @@
 
 const fp = require('fastify-plugin')
 const mailgun = require('mailgun-js')
+const axios = require('axios')
 const Schemas = require('./schemas')
 
 async function fastifyMailgun (fastify, options, done) {
-  const { apiKey, domain, prefix = '/mailgun' } = options
+  const { apiKey, domain, prefix = '/mailgun', validateWebhooks = true } = options
   const client = options.client || mailgun({ apiKey, domain })
   const handler = options.handler.bind(fastify)
 
+  const getStoredMessage = async function ({ url, mime = false }) {
+    try {
+      const headers = mime ? { Accept: 'message/rfc2822' } : {}
+      const auth = { username: 'api', password: client.apiKey }
+      const { data } = await axios.get(url, { auth, headers })
+      return data
+    } catch (err) {
+      return null
+    }
+  }
+
   const hooksHandler = async function (req, reply) {
     const { timestamp, token, signature } = req.body.signature
-    const isValid = true // TODO : || client.validateWebhook(timestamp, token, signature)
+    const isValid = (validateWebhooks) ? client.validateWebhook(timestamp, token, signature) : true
     if (!isValid) { throw Error('Invalid webhook signature') }
     const result = await handler(req.body['event-data'])
     return result
   }
 
-  const messageReSend = async function () {
-    console.log('RE-SENDING....')
-    // ..
-    return true
-  }
-
-  fastify.decorate('mailgun', { client, messageReSend })
+  fastify.decorate('mailgun', { client, getStoredMessage })
   fastify.post(`${prefix}/webhook`, { schema: Schemas.WEBHOOK }, hooksHandler)
   done()
 }
